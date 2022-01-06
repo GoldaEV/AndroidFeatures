271cfa5a5b7b5748d89312a4598453343da96dd4
GoldaEV <goldaev@gmail.com>
2022-01-06 13:45:57 +0300

    Create hide_keyboard.kt

---
+++ hide_keyboard.kt
@@ -0,0 +1,7 @@
+ private void hideKeyboard() {
+        View view = getCurrentFocus();
+        if (view != null) {
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+    }
