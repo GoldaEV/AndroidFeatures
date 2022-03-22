1e7de3bc7a37f44ac97247c03e5c5f01309fd83a
GoldaEV <goldaev@gmail.com>
2022-03-22 09:29:44 +0300

    Create BottomSheetDialogFragment_EXPANDED.kt

---
+++ BottomSheetDialogFragment_EXPANDED.kt
@@ -0,0 +1,10 @@
+    override fun onStart() {
+        super.onStart()
+        dialog?.let {
+            val bottomSheet =
+                it.findViewById<View>(com.google.android.material.R.id.design_bottom_sheet) as FrameLayout
+            val behavior = BottomSheetBehavior.from(bottomSheet)
+            behavior.peekHeight = Resources.getSystem().displayMetrics.heightPixels
+            behavior.state = BottomSheetBehavior.STATE_EXPANDED
+        }
+    }
